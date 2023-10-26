679c77cf467998684e9a6c8cdd34a15fe377f477
George <georgie.george7@gmail.com>
2023-10-26 11:43:26 +0400

    Add Navbar to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import TodoApp from './components/TodoApp';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <TodoApp />
     </div>
   );
